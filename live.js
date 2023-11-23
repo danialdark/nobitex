cab43fa38524c71409bdec0fe785bf254987500a
Danial <danialdark12098@gmail.com>
2023-11-23 12:38:22 +0330

    chaning

--- live.js
+++ live.js
@@ -6,7 +6,7 @@ const db = require('./db'); // Adjust the path as needed
 const Redis = require('ioredis');
 
 const redis = new Redis({
-    host: '91.107.160.210',
+    host: 'localhost',
     port: '6379',
     password: 'D@n!@l12098',
     enableCompression: true,
