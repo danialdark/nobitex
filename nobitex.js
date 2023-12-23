c00c275fae2c083c90676cd15ee1ebcb85a3008f
Danial <danialdark12098@gmail.com>
2023-12-23 09:24:10 +0330

    changing it

--- nobitex.js
+++ nobitex.js
@@ -17,20 +17,20 @@ setInterval(getSpotSymbols, 86400000);
 
 // for getting symbols name
 const getHistory = require('./history.js');
+const refresher = require('./refresher.js');
 
 
 const { getLive, updateRedisPrice } = require('./newLive.js');
 
-// // this will get all active symbols and start streaming
+// this will get all active symbols and start streaming
 
 getSpotSymbols().then((data) => {
-
     getLive(data);
-
-
 });
 
-setTimeout(() => {
+
+setTimeout(async () => {
+    refresher(["all"], ["all"], 4);
     console.log("refreshing the app");
     process.exit(1);
 
